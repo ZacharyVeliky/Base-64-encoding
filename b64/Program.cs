182115ccdad46461c07a2618cf3ff24b65354e5b
zachary veliky <zacharyv2546@gmail.com>
2020-08-06 15:19:22 -0400

    fixed problem where multiple different encodings were not possible on one run of the program

--- b64/Program.cs
+++ b64/Program.cs
@@ -11,35 +11,54 @@ namespace b64
     class Program
     {
         public static string Base64Encode(string plainText)
-                {
-                    var plainTextBytes = System.Text.Encoding.UTF8.GetBytes(plainText);
-                    return System.Convert.ToBase64String(plainTextBytes);
-                }
+            {
+                var plainTextBytes = System.Text.Encoding.UTF8.GetBytes(plainText);
+                return System.Convert.ToBase64String(plainTextBytes);
+            }
         public static string Base64Decode(string base64EncodedData)
-        {
-            var base64EncodedBytes = System.Convert.FromBase64String(base64EncodedData);
-            return System.Text.Encoding.UTF8.GetString(base64EncodedBytes);
-        }
+            {
+                var base64EncodedBytes = System.Convert.FromBase64String(base64EncodedData);
+                return System.Text.Encoding.UTF8.GetString(base64EncodedBytes);
+            }
+
 
         [STAThread]
         static void Main(string[] args)
         {
             Top:
+            int i = 0;
+            string temp1;
+            string temp2;
+            
             Console.WriteLine("[1] Encode");
             Console.WriteLine("[2] Decode");
             
             string input = Console.ReadLine();
+
+            Console.WriteLine("How many rounds of encoding/decoding should be run");
+            
+            int rounds = Convert.ToInt32(Console.ReadLine());
+            
             Console.WriteLine("");
+
             if(input == "1")
             {
                 Console.WriteLine("Enter text to be encoded");
                 string inputString = Console.ReadLine();
                 Console.WriteLine(" | ");
                 Console.WriteLine(" V ");
-                Console.WriteLine(Base64Encode(inputString) + "\n");
-
-                Clipboard.SetText(Base64Encode(inputString));
-                Console.WriteLine("Encoded string coppied to clipbord. Press enter to continue.");
+                temp1 = inputString;
+                while (i < rounds){
+                    temp2 = Base64Encode(temp1);
+                    temp1 = temp2;
+                    i++;
+                    if (i == rounds){
+                        Console.WriteLine(temp1);
+                        Clipboard.SetText(temp1);
+                        }
+                }
+                
+                Console.WriteLine("Encoded string coppied to clipbord. Press enter to continue.\n");
                 Console.ReadKey();
                 goto Top;
             }
@@ -49,18 +68,24 @@ namespace b64
                 string inputString = Console.ReadLine();
                 Console.WriteLine(" | ");
                 Console.WriteLine(" V ");
-                Console.WriteLine(Base64Decode(inputString) + "\n");
+                temp1 = inputString;
+                while (i < rounds){
+                    temp2 = Base64Decode(temp1);
+                    temp1 = temp2;
+                    i++;
+                    if (i == rounds){
+                        Console.WriteLine(temp1);
+                        Clipboard.SetText(temp1);
+                        }
+                }
 
-                Clipboard.SetText(Base64Encode(inputString));
-                Console.WriteLine("Encoded string coppied to clipbord. Press enter to continue.");
+                Console.WriteLine("Encoded string coppied to clipbord. Press enter to continue.\n");
                 Console.ReadKey();
                 goto Top;
             }
         }
         public void Update()
         {
-            
-
 
         }
     }
