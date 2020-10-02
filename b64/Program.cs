5198653c9a7fd8b504f39a67bc8e81fe7fbd3d08
zachary veliky <zacharyv2546@gmail.com>
2020-10-02 13:58:02 -0400

    Moved to a while loop for the main function - .exe file is smaller. - program uses less ram. Added an exit function - no chance of error when the program is closed using the function.

--- b64/Program.cs
+++ b64/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Data;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -11,89 +12,94 @@ namespace b64
     class Program
     {
         public static string Base64Encode(string plainText)
-            {
-                var plainTextBytes = System.Text.Encoding.UTF8.GetBytes(plainText);
-                return System.Convert.ToBase64String(plainTextBytes);
-            }
+        {
+            var plainTextBytes = System.Text.Encoding.UTF8.GetBytes(plainText);
+            return System.Convert.ToBase64String(plainTextBytes);
+        }
         public static string Base64Decode(string base64EncodedData)
-            {
-                var base64EncodedBytes = System.Convert.FromBase64String(base64EncodedData);
-                return System.Text.Encoding.UTF8.GetString(base64EncodedBytes);
-            }
+        {
+            var base64EncodedBytes = System.Convert.FromBase64String(base64EncodedData);
+            return System.Text.Encoding.UTF8.GetString(base64EncodedBytes);
+        }
 
         [STAThread]
         static void Main(string[] args)
         {
-            Top:
+            int loop = 0;
+            while (loop != 1) {
             int i = 0;
             string temp1;
             string temp2;
-            
+
             Console.WriteLine("[1] Encode");
             Console.WriteLine("[2] Decode");
-            
+            Console.WriteLine("[3] Exit");
+
             string input = Console.ReadLine();
 
-            Console.WriteLine("How many rounds of encoding/decoding should be run. (Default is 1)");
-
             int rounds;
+                if (input != "3")
+                {
+                    Console.WriteLine("How many rounds of encoding/decoding should be run. (Default is 1)");
+                    string tempInput = Console.ReadLine();
+                    if (tempInput.Length < 1)
+                        rounds = 1;
 
-            string tempInput = Console.ReadLine();
+                    else
+                        rounds = Convert.ToInt32(tempInput);
 
-            if (tempInput.Length < 1)
-                rounds = 1;
 
-            else
-                rounds = Convert.ToInt32(tempInput);
-            
-            Console.WriteLine("");
+                    Console.WriteLine("");
 
-            if(input == "1")
-            {
-                Console.WriteLine("Enter text to be encoded");
-                string inputString = Console.ReadLine();
-                Console.WriteLine(" | ");
-                Console.WriteLine(" V ");
-                temp1 = inputString;
-                while (i < rounds){
-                    temp2 = Base64Encode(temp1);
-                    temp1 = temp2;
-                    i++;
-                    if (i == rounds){
-                        Console.WriteLine(temp1);
-                        Clipboard.SetText(temp1);
+                    if (input == "1")
+                    {
+                        Console.WriteLine("Enter text to be encoded");
+                        string inputString = Console.ReadLine();
+                        Console.WriteLine(" | ");
+                        Console.WriteLine(" V ");
+                        temp1 = inputString;
+                        while (i < rounds)
+                        {
+                            temp2 = Base64Encode(temp1);
+                            temp1 = temp2;
+                            i++;
+                            if (i == rounds)
+                            {
+                                Console.WriteLine(temp1);
+                                Clipboard.SetText(temp1);
+                            }
                         }
-                }
-                
-                Console.WriteLine("Encoded string coppied to clipbord. Press enter to continue.\n");
-                Console.ReadKey();
-                goto Top;
-            }
-            else
-            {
-                Console.WriteLine("Enter text to be decoded");
-                string inputString = Console.ReadLine();
-                Console.WriteLine(" | ");
-                Console.WriteLine(" V ");
-                temp1 = inputString;
-                while (i < rounds){
-                    temp2 = Base64Decode(temp1);
-                    temp1 = temp2;
-                    i++;
-                    if (i == rounds){
-                        Console.WriteLine(temp1);
-                        Clipboard.SetText(temp1);
+
+                        Console.WriteLine("Encoded string coppied to clipbord. Press enter to continue.\n");
+                        Console.ReadKey();
+                    }
+                    else if (input == "2")
+                    {
+                        Console.WriteLine("Enter text to be decoded");
+                        string inputString = Console.ReadLine();
+                        Console.WriteLine(" | ");
+                        Console.WriteLine(" V ");
+                        temp1 = inputString;
+                        while (i < rounds)
+                        {
+                            temp2 = Base64Decode(temp1);
+                            temp1 = temp2;
+                            i++;
+                            if (i == rounds)
+                            {
+                                Console.WriteLine(temp1);
+                                Clipboard.SetText(temp1);
+                            }
                         }
+                        Console.WriteLine("Encoded string coppied to clipbord. Press enter to continue.\n");
+                        Console.ReadKey();
+                    }
                 }
-
-                Console.WriteLine("Encoded string coppied to clipbord. Press enter to continue.\n");
-                Console.ReadKey();
-                goto Top;
+                else
+                {
+                    break;
+                }
             }
-        }
-        public void Update()
-        {
-
         }
     }
 }
