36551c7fbfed29cca4330c46c9ec31e366d7fbb7
zachary veliky <zacharyv2546@gmail.com>
2020-10-20 23:32:52 -0400

    Removed unnecessary code

--- b64/Program.cs
+++ b64/Program.cs
@@ -1,10 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Data;
-using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace b64
